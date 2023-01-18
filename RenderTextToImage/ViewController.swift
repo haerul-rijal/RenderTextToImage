faad14f528be65e80b9f03f3b51d3fd54af59577
Haerul Rijal <haerul.rijal@tokopedia.com>
2023-01-18 14:59:30 +0800

    Add project template

---
+++ RenderTextToImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RenderTextToImage
+//
+//  Created by haerul.rijal on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
